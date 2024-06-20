ad3dab9eae47adb283bc9ea040bde60e13ecc251
Mohamedossama44 <mohamedossama045@gmail.com>
2024-06-21 00:50:47 +0300

    commit

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 display.textContent = equation;
             }
 
+
             if (operator) {
                 currentInput = '';
                 equation += ` ${operator} `;
